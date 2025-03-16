8f0a581cc0ce8cb8a1f53a454328da403e214dad
Danila <laorant8@gmail.com>
2025-03-16 23:11:31 +0700

    Added tests for SavingAccount

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
 
+    //Тесты метода add
+
     @Test
     public void shouldAddLessThanMaxBalance() {
         SavingAccount account = new SavingAccount(
@@ -18,16 +20,124 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+
+    @Test
+    public void shouldAddMoreThanMaxBalance() {
+        int initialBalance = 1_000;
+        int minBalance = 100;
+        int maxBalance = 5_000;
+        int rate = 5;
+
+        SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
+
+        // Пополняем счет balance > maxBalance
+        account.add(5_000);
+        Assertions.assertEquals(initialBalance, account.getBalance()); // Начальный баланс не должен измениться
+
+    }
+
+    @Test
+    public void shouldAddMoreNegativeAmount() {
+        int initialBalance = 1_000;
+
+        SavingAccount account = new SavingAccount(initialBalance, 0, 5_000, 5);
+
+        account.add(-100); // Попытка пополнить на отрицательную сумму
+        Assertions.assertEquals(initialBalance, account.getBalance()); // Начальный баланс не должен измениться
+    }
+
+
+    //Тест метода yearChange
+
     @Test
     public void testYearChange() {
 
-        SavingAccount account = new SavingAccount(250,  0, 15_000,15);
+        SavingAccount account = new SavingAccount(
+                200,
+                0,
+                15_000,
+                15);
 
         // Проверка расчета процентов
-        int expected = 37;
+        int expected = 30;
         int actual = account.yearChange();
 
         // Проверка, что фактический результат равен ожидаемому
         Assertions.assertEquals(expected, actual);
+    }
+    // Тест метода SavingAccount
+
+    @Test
+    public void testNewSavingAccountMinEqualsMax() {
+
+        int initialBalance = 1_000;
+        int minBalance = 1_000; // minBalance = maxBalance
+        int maxBalance = 1_000;
+        int rate = 5;
+
+        // Проверяем, что конструктор работает корректно, если initialBalance равен minBalance и maxBalance
+        SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
+        Assertions.assertEquals(initialBalance, account.getBalance());
+        Assertions.assertEquals(minBalance, account.getMinBalance());
+        Assertions.assertEquals(maxBalance, account.getMaxBalance());
+
+        // Проверяем, что конструктор выбрасывает исключение, если initialBalance не равен minBalance и maxBalance;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(500, 1_000, 2_000, 5);
+        });
+    }
+
+    @Test
+    public void testNewSavingAccountNegativRate() {
+
+        // Проверяем, что конструктор выбрасывает исключение, если rate < 0
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(100, 50, 200, -5);
+        });
+    }
+
+    @Test
+    public void testNewSavingAccountMinBalanceGreaterThanMaxBalance() {
+
+        //Проверяем, что конструктор выбрасывает исключение, если minBalance > maxBalance.
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(500, 2000, 1000, 5);
+        });
+    }
+
+    //Тест метода pay
+
+    @Test
+    public void testPay() {
+        SavingAccount account = new SavingAccount(1_000, 0, 10_000, 0);
+
+        account.pay(500);
+
+        Assertions.assertEquals(1_000 - 500, account.getBalance());
+
+    }
+
+    @Test
+    public void testPayMoreThanMinBalance() {
+        int initialBalance = 1_000;
+
+        SavingAccount account = new SavingAccount(initialBalance, 500, 10_000, 0);
+
+        account.pay(600);
+
+        Assertions.assertEquals(initialBalance, account.getBalance());
+
+    }
+
+    @Test
+    public void testPayMoreNegativeAmount() {
+        int initialBalance = 1_000;
+
+        SavingAccount account = new SavingAccount(initialBalance, 500, 10_000, 0);
+
+        account.pay(-100);
+
+        Assertions.assertEquals(initialBalance, account.getBalance());
+
     }
 }
